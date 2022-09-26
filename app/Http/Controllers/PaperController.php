09821bc28158ef0392dd67a19faca9d5231047ed
hirohito0902 <hiromax6@outlook.jp>
2022-09-26 13:40:23 +0900

    基礎実装

---
+++ app/Http/Controllers/PaperController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Paper;
+
+
+class PaperController extends Controller
+{
+    public function index(Paper $paper)
+    {
+        return $paper->get();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaperController; 
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,10 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+
+Route::get('/papers', [PaperController::class, 'index']);
